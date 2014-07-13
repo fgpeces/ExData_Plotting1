e75f1a2df5d0a13206766d634562a2309b9cca23
fgpeces <javier.gpeces@gmail.com>
2014-07-13 12:32:55 +0200

    Added my third script and plot, take into account the name of the days is in Spanish instead of english, due to my R configuration

--- R_files/plot1.R
+++ R_files/plot1.R
@@ -1,4 +1,3 @@
-#exp_p1 only contains point for the dates concerned in this exercise. 
 #read csv file
 x <- read.table("household_power_consumption.txt",header=TRUE,sep=";",na.strings="?",skip=66636,nrows=2880,col.names=c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 
